903a43a3ecd0a5f970465965c90606576669f115
Dmitry Klyushkov <charlesfxa@gmail.com>
2023-06-01 22:20:48 +0300

    Dracula color scheme

--- .config/fish/conf.d/tide.fish
+++ .config/fish/conf.d/tide.fish
@@ -7,30 +7,31 @@ set -g tide_left_prompt_prefix "╭─"
 set -g tide_left_prompt_separator_diff_color \ue0b0
 set -g tide_left_prompt_suffix \ue0b0
 # PWD Settings
-set -g tide_pwd_bg_color 859900
-set -g tide_pwd_color_dirs 073642
-set -g tide_pwd_color_anchors 073642
+set -g tide_pwd_bg_color 78f09a
+set -g tide_pwd_color_dirs 21222c
+set -g tide_pwd_color_anchors 21222c
 # Git Settings
-set -g tide_git_bg_color b58900
-set -g tide_git_bg_color_unstable b58900
-set -g tide_git_color_branch 073642
+set -g tide_git_bg_color f1fa8c
+set -g tide_git_bg_color_unstable f1fa8c
+set -g tide_git_color_branch 21222c
 set -g tide_git_icon \ue725 
-set -g tide_git_color_conflicted 073642
-set -g tide_git_color_dirty 073642
-set -g tide_git_color_operation 073642
-set -g tide_git_color_staged 073642
-set -g tide_git_color_stash 073642
-set -g tide_git_color_untracked 073642
-set -g tide_git_color_upstream 073642
+set -g tide_git_color_conflicted 21222c
+set -g tide_git_color_dirty 21222c
+set -g tide_git_color_operation 21222c
+set -g tide_git_color_staged 21222c
+set -g tide_git_color_stash 21222c
+set -g tide_git_color_untracked 21222c
+set -g tide_git_color_upstream 21222c
 
 set -g tide_character_icon "╰─>"
-set -g tide_character_color 859900
+set -g tide_character_color 78f09a
 
 # Right Prompt
 set -g tide_right_prompt_items time
 set -g tide_right_prompt_prefix \ue0b6
 set -g tide_right_prompt_suffix \ue0b4
 # Time Settings
-set -g tide_time_bg_color 2aa198
-set -g tide_time_color 002b36
+set -g tide_time_bg_color bd93f9
+set -g tide_time_color 21222c
 set -g tide_time_format "%H:%M:%S"
+
