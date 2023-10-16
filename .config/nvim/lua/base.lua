fd82b806dd293b2f6d9b0c67f2bc4e6597c7a070
Dmitry Klyushkov <DmitryKlyushkov@gmail.com>
2023-10-16 21:42:44 +0300

    nvim added treesitter code folding configs

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -35,6 +35,34 @@ vim.opt.wildignore:append { '*/node_modules/*' }    -- Folders/Files ignored by 
 vim.opt.formatoptions:append { 'r' }
 vim.opt.formatoptions:append { 'r' }
 
+-- Treesitter code folding
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+local M = {}
+-- function to create a list of commands and convert them to autocommands
+function M.nvim_create_augroups(definitions)
+  for group_name, definition in pairs(definitions) do
+    vim.api.nvim_command('augroup ' .. group_name)
+    vim.api.nvim_command('autocmd!')
+    for _, def in ipairs(definition) do
+      local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
+      vim.api.nvim_command(command)
+    end
+    vim.api.nvim_command('augroup END')
+  end
+end
+
+local autoCommands = {
+  -- other autocommands
+  open_folds = {
+    { "BufReadPost,FileReadPost", "*", "normal zR" }
+  }
+}
+
+M.nvim_create_augroups(autoCommands)
+
 -- Helpers --
 -- Key-mapping function
 local function map(mode, lhs, rhs, opts)
