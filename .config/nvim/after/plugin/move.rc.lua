a7f2f7d01e481181a71fe587900e0e54d3481b43
Dmitry Klyushkov <DmitryKlyushkov@gmail.com>
2024-02-03 04:27:17 +0300

    nvim move plugin, added setup configs

--- .config/nvim/after/plugin/move.rc.lua
+++ .config/nvim/after/plugin/move.rc.lua
@@ -1,10 +1,21 @@
-
 -- Helpers
 local function map(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.keymap.set(mode, lhs, rhs, options)
 end
+
+require('move').setup({
+  line = {
+    enable = true, -- Enables line movement
+    indent = true  -- Toggles indentation
+  },
+  block = {
+    enable = true, -- Enables block movement
+    indent = true  -- Toggles indentation
+  },
+})
+
 -- Normal-mode commands
 map('n', '<A-j>', ':MoveLine(1)<CR>')
 map('n', '<A-k>', ':MoveLine(-1)<CR>')
@@ -12,3 +23,4 @@ map('n', '<A-k>', ':MoveLine(-1)<CR>')
 -- Visual-mode commands
 map('v', '<A-j>', ':MoveBlock(1)<CR>')
 map('v', '<A-k>', ':MoveBlock(-1)<CR>')
+
