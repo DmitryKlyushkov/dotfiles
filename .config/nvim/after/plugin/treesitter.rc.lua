1f21b22f724dc47a41e68ac0b112778fe4d63ed1
Dmitry Klyushkov <DmitryKlyushkov@gmail.com>
2024-05-20 20:47:02 +0300

    fix nvim treesitter config

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -29,9 +29,9 @@ ts.setup {
     "swift",
     "lua",
   },
-  autotag = {
-    enable = true,
-  },
+  -- autotag = {
+  --   enable = true,
+  -- },
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
