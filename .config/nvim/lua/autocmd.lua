f893756b192cb26dd3b5e1ef83a2221d37fbb025
Dmitry Klyushkov <DmitryKlyushkov@gmail.com>
2025-10-06 18:56:58 +0300

    feat: nvim add autocommand to stop automatically adding a comment line after pressing Enter or O

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,12 +1,20 @@
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
+	pattern = "*",
+	command = "set nopaste",
 })
 
 -- Kill eslint_d and prettierd processes when leaving Neovim
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  callback = function()
-    vim.fn.jobstart('killall eslint_d prettierd', { detach = true })
-  end,
+	callback = function()
+		vim.fn.jobstart("killall eslint_d prettierd", { detach = true })
+	end,
+})
+
+-- Do not continue comments in Normal/Insert mode
+vim.api.nvim_create_autocmd({ "BufWinEnter", "FileType" }, {
+	callback = function()
+		vim.opt_local.formatoptions:remove("r") -- Do not continue comments when pressing <Enter> in Insert mode. `:h fo-r`
+		vim.opt_local.formatoptions:remove("o") -- Do not continue comments when pressing 'o' or 'O' in Normal mode. `:h fo-o`
+	end,
 })
