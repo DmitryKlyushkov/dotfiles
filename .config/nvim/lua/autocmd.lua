d501d098ca86c5934d8fb55cef6b62ae56b43930
Dmitry Klyushkov <DmitryKlyushkov@gmail.com>
2025-10-19 13:35:24 +0300

    feat: nvim add autocmd to use vertical split for :help by default

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -18,3 +18,17 @@ vim.api.nvim_create_autocmd({ "BufWinEnter", "FileType" }, {
 		vim.opt_local.formatoptions:remove("o") -- Do not continue comments when pressing 'o' or 'O' in Normal mode. `:h fo-o`
 	end,
 })
+
+-- Always use vertical split for :help
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	pattern = "*.txt", -- Match help buffers (help files use .txt extension)
+	callback = function()
+		-- Check if the buffer is a help buffer
+		if vim.bo.filetype == "help" then
+			-- Move the current window to a vertical split on the right
+			vim.cmd("wincmd L")
+			-- Optionally set a fixed width for the help window
+			vim.cmd("vertical resize 80")
+		end
+	end,
+})
